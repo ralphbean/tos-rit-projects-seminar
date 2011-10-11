46662e058263ef2b1affe2d4b0243fa317ebc41e
Ralph Bean <ralph.bean@gmail.com>
2011-10-11 14:11:37 -0400

    Added package stuff for rtfd.

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""Setuptools setup file"""
+
+import sys, os
+import multiprocessing, logging
+
+from setuptools import setup
+
+setup(
+    name='rit.floss',
+    version='0.1',
+    description="Materials for teaching open source at RIT",
+    install_requires=[
+        'Sphinx',
+        'cloud_sptheme',
+    ],
+    url = "http://teachingopensource.org/index.php/RIT",
+    download_url = "https://github.com/ralphbean/tos-rit-projects-seminar",
+    author='Ralph Bean',
+    author_email='ralph.bean@gmail.com',
+    license='GPLv3+',
+    packages = [],
+    namespace_packages = [],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers = [],
+)
