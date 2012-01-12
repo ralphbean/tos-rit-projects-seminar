3220709e7df9bb25d6789e984ffa9150b2fa5287
Nathaniel Case <Qalthos@gmail.com>
2012-01-12 14:15:29 -0500

    Scrape planet feeds into students.yaml

---
+++ lib/big_brother/blog_check.py
@@ -0,0 +1,40 @@
+import pprint as pp
+import yaml
+
+
+planet_file = '../../planet/config.ini'
+yaml_file = '../../data/students.yaml'
+
+def ini_to_yaml():
+    feeds = {}
+    with open(planet_file) as planet:
+        planet_data = iter(planet.readlines())
+
+    while not planet_data.next() == '####\n':
+        pass
+
+    current = {}
+    for line in planet_data:
+        if line[0] == '[':
+            current['feed'] = line[1:-1]
+        elif not line == '\n':
+            bits = line.split(' = ')[1].split(None, 1)
+            handle = bits[0]
+            current['name'] = bits[1].strip('()\n')
+            feeds[handle] = current
+            current = {}
+
+    with open(yaml_file) as students:
+        student_data = yaml.load(students.read(-1))
+
+    for student in student_data:
+        if not student['irc'] in feeds:
+            print('Student %s not found!' % student['irc'])
+            continue
+
+        student['feed'] = feeds[student['irc']]['feed']
+
+    with open(yaml_file, 'w') as students:
+        yaml.dump(student_data, students, default_flow_style=False)
+
+ini_to_yaml()
